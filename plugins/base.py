9bb080d1fd5f3c4e346f8390fc9f23d85515dc9b
Brian <bmbove@gmail.com>
2015-05-17 23:12:58 -0700

    adds network plugin for really basic wifi info adds some more functionality to base plugin class

--- plugins/base.py
+++ plugins/base.py
@@ -1,3 +1,4 @@
+import re
 import time
 from threading import Thread
 
@@ -6,20 +7,20 @@ class PluginBase(Thread):
     daemon = True
 
     def __init__(self, out_q, config):
+
         self.out_q = out_q
+        self.config = self.configure(config)
+        self.config['cache_time'] = float(self.config.get('cache_time', 1.0))
 
-        if not config.get('cache_time', False):
-            config['cache_time'] = 1.0
-        else:
-            config['cache_time'] = float(config['cache_time'])
-
-        self.config = config
         self._display = {
-            'full_text': '',
+            'full_text': ' ',
             'color': '#%s' % config.get('color', 'FFFFFF')
         }
+
         super(PluginBase, self).__init__()
 
+    def configure(self, config):
+        return config
 
     def set_text(self, s):
         self._display['full_text'] = s
@@ -27,8 +28,23 @@ class PluginBase(Thread):
     def set_color(self, s):
         self.display['color'] = s
 
+    def set_cache_time(self, d):
+        self.config['cache_time'] = d
+
     def update(self):
         pass
+
+    def fix_format(self, f):
+        return re.sub(r'{(?P<item>[a-zA-Z0-9_]+)}', r'%(\g<item>)s', f)
+
+    def read_file(self, filename, start=0):
+        data = ""
+        with open(filename, 'r') as fh:
+            for i in range(start):
+                fh.readline()
+            for line in fh:
+                data += line
+        return data
 
     def run(self):
         while True:
---
+++ plugins/network.py
@@ -0,0 +1,84 @@
+"""
+get_ip source:
+http://stackoverflow.com/a/9267833
+"""
+import array
+import fcntl
+import socket
+import struct
+
+from .base import PluginBase
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sockfd = sock.fileno()
+calls = {
+    'SIOCGIFADDR': 0x8915,
+    'SIOCGIWESSID': 0x8B1B,
+}
+
+class NetworkPlugin(PluginBase):
+
+    def configure(self, config):
+        if not config.get('cache_time', False):
+            config['cache_time'] = 2.0
+        if not config.get('format', False):
+            config['format'] = '{essid} {ip}'
+        config['format'] = self.fix_format(config['format'])
+        return config
+
+    def get_ip(self, iface):
+        iface = iface.encode('utf-8')
+        ifreq = struct.pack('16sH14s', iface, socket.AF_INET, b'\x00'*14)
+        try:
+            res = fcntl.ioctl(sockfd, calls['SIOCGIFADDR'], ifreq)
+        except:
+            return None
+        ip = struct.unpack('16sH2x4s8x', res)[2]
+        return socket.inet_ntoa(ip)
+
+    def get_essid(self, interface):
+        maxLength = {
+            "interface": 16,
+            "essid": 32
+        }
+        """Return the ESSID for an interface, or None if we aren't connected."""
+        interface = interface.ljust(maxLength["interface"], "\0").encode('utf-8')
+        essid = array.array("B", "\0".encode('utf-8') * maxLength["essid"])
+        essidPointer, essidLength = essid.buffer_info()
+        request = array.array("B",
+            interface +           
+            struct.pack("PHH",   
+                essidPointer,   
+                essidLength,   
+                0)            
+        )                    
+        fcntl.ioctl(sock.fileno(), calls["SIOCGIWESSID"], request)
+        name = essid.tostring().decode('utf-8').rstrip('\0')
+        if name:
+            return name
+        return "-"
+
+    def general_info(self):
+        ifaces = {}
+        info = self.read_file('/proc/net/wireless', start=2)
+        for line in info.split("\n"):
+            l_arr = [item for item in line.strip().split(" ") if item]
+            if len(l_arr) > 0:
+                ifaces[l_arr[0].replace(":", "")] = {
+                    'status': l_arr[1],
+                    'link': l_arr[2],
+                    'level': l_arr[3],
+                    'noise': l_arr[4],
+                }
+        self.ifaces = ifaces
+
+    def update(self):
+        self.general_info()
+        if 'interface' not in self.config:
+            for key in self.ifaces.keys():
+                self.config['interface'] = key
+                break
+        iface = self.config['interface']
+        ip = self.get_ip(iface)
+        essid = self.get_essid(iface)
+        self.set_text(self.config['format'] % locals())
