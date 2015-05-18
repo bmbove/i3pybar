141df4366f933897c82998a713979d1cbf10910a
brian <bmbove@gmail.com>
2015-05-17 17:41:02 -0700

    plugins/base.py: added caching to plugin output

--- plugins/base.py
+++ plugins/base.py
@@ -1,11 +1,18 @@
+import time
+
 class PluginBase(object):
 
     def __init__(self, config):
+        if not config.get('cache_time', False):
+            config['cache_time'] = 1.0
+        else:
+            config['cache_time'] = float(config['cache_time'])
         self.config = config
         self._display = {
             'full_text': '',
             'color': '#%s' % config.get('color', 'FFFFFF')
         }
+        self.last_update = 0
 
     def set_text(self, s):
         self._display['full_text'] = s
@@ -17,5 +24,7 @@ class PluginBase(object):
         pass
 
     def output(self):
-        self.run()
+        if (time.time() - self.last_update) > self.config['cache_time']:
+            self.run()
+            self.last_update = time.time()
         return self._display 
