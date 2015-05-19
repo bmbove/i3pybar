52ee6ff013d12fc357590bd9a40a130ce49fde64
Brian Bove <bmbove@gmail.com>
2015-05-19 15:46:56 -0700

    cpu.py: added option to pull info from /proc/stat

--- plugins/cpu.py
+++ plugins/cpu.py
@@ -1,3 +1,4 @@
+import os.path
 import time
 
 from .base import PluginBase
@@ -7,18 +8,32 @@ class CPUPlugin(PluginBase):
 
     def configure(self):
         defaults = {
-            'format': '{cpu}%%'
+            'format': '{cpu}%%',
+            'use_proc': False,
         }
         self.last_usage = []
         self.last_time = 0
+        self.source = '/sys/fs/cgroup/cpu/cpuacct.usage_percpu'
+        if not os.path.isfile(self.source) or bool(defaults.use_proc):
+            self.source = '/proc/stat'
         return defaults
 
     def get_usage(self):
-        usage = self.read_file('/sys/fs/cgroup/cpu/cpuacct.usage_percpu').strip()
-        usage = [int(ns) for ns in usage.split(" ")]
-        total = int(self.read_file('/sys/fs/cgroup/cpu/cpuacct.usage'))
-        usage.append(total)
-        return usage 
+        stats = []
+        usage = self.read_file(self.source).strip()
+        if 'sys' in self.source:
+            stats = [int(ns) for ns in usage.split(" ")]
+            total = int(self.read_file('/sys/fs/cgroup/cpu/cpuacct.usage'))
+            stats.append(total)
+            stats.insert(0, stats.pop())
+        else:
+            # not sure of the accuracy of this..
+            usage_l = [line for line in usage.split("\n") if 'cpu' in line]
+            for i in range(0, len(usage_l)):
+                info = [item for item in usage_l[i].split(" ") if item]
+                busy = (int(info[1]) + int(info[2]) + int(info[3])) * 10000000
+                stats.append(busy)
+        return stats 
 
     def calc_percents(self, usage):
 
@@ -39,9 +54,9 @@ class CPUPlugin(PluginBase):
         stats = {}
         percents = self.calc_percents(self.get_usage())
 
-        for i in range(0, len(percents) - 1):
-            stats['cpu%d' % i] = percents[i]
-        stats['cpu'] = percents[len(percents)-1]
+        stats['cpu'] = percents[0]
+        for i in range(1, len(percents) - 1):
+            stats['cpu%d' % (i-1)] = percents[i]
 
         locals().update(stats)
         self.set_text(self.config['format'] % locals())
