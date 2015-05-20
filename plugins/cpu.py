845ceae8b0403667942da598d248515a0f2f4bd6
brian <bmbove@gmail.com>
2015-05-19 18:25:38 -0700

    cpu.py: changed info source to /proc/stat

--- plugins/cpu.py
+++ plugins/cpu.py
@@ -9,51 +9,45 @@ class CPUPlugin(PluginBase):
     def configure(self):
         defaults = {
             'format': '{cpu}%%',
-            'use_proc': False,
         }
-        self.last_usage = []
+        self.source = '/proc/stat'
+        self.last_usage = [0] * (self.num_cpus() + 1)
         self.last_time = 0
-        self.source = '/sys/fs/cgroup/cpu/cpuacct.usage_percpu'
-        if not os.path.isfile(self.source) or bool(defaults['use_proc']):
-            self.source = '/proc/stat'
         return defaults
 
-    def get_usage(self):
-        stats = []
+    def num_cpus(self):
+        cores = -1
+        for line in self.read_file(self.source).split("\n"):
+            if 'cpu' in line:
+                cores += 1
+        return cores
+
+    """
+    /proc/stat isn't as 'accurate' as getting information from sysfs as far
+    as I can tell (sysfs cpu data is in nanoseconds), but... CPU percentage
+    isn't an exact science anyways.
+    """
+    def proc_usage(self):
+        percents = []
+        cur_time = time.time()
         usage = self.read_file(self.source).strip()
-        if 'sys' in self.source:
-            stats = [int(ns) for ns in usage.split(" ")]
-            total = int(self.read_file('/sys/fs/cgroup/cpu/cpuacct.usage'))
-            stats.append(total)
-            stats.insert(0, stats.pop())
-        else:
-            # not sure of the accuracy of this..
-            usage_l = [line for line in usage.split("\n") if 'cpu' in line]
-            for i in range(0, len(usage_l)):
-                info = [item for item in usage_l[i].split(" ") if item]
-                busy = (int(info[1]) + int(info[2]) + int(info[3])) * 10000000
-                stats.append(busy)
-        return stats 
+        d_time = cur_time - self.last_time
+        self.last_time = cur_time
+        usage_l = [line for line in usage.split("\n") if 'cpu' in line]
+        for i in range(0, len(usage_l)):
+            info = [int(item) for item in usage_l[i].split(" ") if ('cpu' not in item and item)]
+            total = sum(info[0:-2])
+            busy = total - info[3] - info[4]
+            d_busy = busy - self.last_usage[i]
+            percent = int((d_busy/(d_time*100)) * 100)
+            percents.append(percent)
+            self.last_usage[i] = busy 
 
-    def calc_percents(self, usage):
-
-        if len(self.last_usage) < len(usage):
-            self.last_usage = [0] * len(usage)
-
-        d_usage = []
-        for i in range(0, len(usage)):
-            d_usage.append(usage[i] - self.last_usage[i])
-
-        dt = (time.time() - self.last_time) * (10**9)
-        percents = [int((dns/dt) * 100) for dns in d_usage]
-        self.last_time = time.time()
-        self.last_usage = usage
-        return percents
+        return percents 
 
     def update(self):
         stats = {}
-        percents = self.calc_percents(self.get_usage())
-
+        percents = self.proc_usage()
         stats['cpu'] = percents[0]
         for i in range(1, len(percents) - 1):
             stats['cpu%d' % (i-1)] = percents[i]
