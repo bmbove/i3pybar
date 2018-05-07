680c776a9bc3fb1c905d6c2a8b9d7789375e9600
Brian Bove <bmbove@gmail.com>
2018-05-07 16:04:36 -0700

    only update pacman thing every 10 min

--- plugins/pacman.py
+++ plugins/pacman.py
@@ -3,9 +3,16 @@
 """
 import re
 import subprocess
+import time
 from .base import PluginBase
+
+# update every 10 minutes
+UPDATE_TIME = 600
  
 class PacmanPlugin(PluginBase):
+
+    update_count = 0
+    last_update = 0
  
     def configure(self):
         defaults = {
@@ -14,9 +21,13 @@ class PacmanPlugin(PluginBase):
         return defaults
 
     def get_update_count(self):
-        lines = subprocess.check_output(["checkupdates"])
-        lines = lines.decode('ascii').strip()
-        return {'updates': len(lines.split("\n"))}
+        now = time.time()
+        if now - self.last_update > UPDATE_TIME:
+            lines = subprocess.check_output(["checkupdates"])
+            lines = lines.decode('ascii').strip()
+            self.update_count = len(lines.split("\n"))
+            self.last_update = now
+        return {'updates': self.update_count}
  
     def update(self):
         res = self.get_update_count()
